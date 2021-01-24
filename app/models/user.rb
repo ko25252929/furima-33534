7fa9bb20df6c1a1e9928ffdcd17d1db8eda1d410
ko25252929 <76517744+ko25252929@users.noreply.github.com>
2021-01-24 16:57:49 +0900

    ユーザー機能コードレビュー修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
 
- with_options presence: true do
-  validates :nickname
-  validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください'} 
-  validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください'} 
-  validates :first_kana_name,format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください'} 
-  validates :last_kana_name,format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください'} 
-  validates :birthday
-  validates :password,format: { with: VALID_PASSWORD_REGEX,message: "は半角英数字混合それぞれ１文字以上含む必要があります"}
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    
+    VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+    validates :password,format: { with: VALID_PASSWORD_REGEX,message: "は半角英数字混合それぞれ１文字以上含む必要があります"}
 
- end
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: '全角文字を使用してください'}  do
+      validates :first_name
+      validates :last_name
+    end
+    
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください'}  do
+      validates :first_kana_name
+      validates :last_kana_name
+    end
+  end
 end
